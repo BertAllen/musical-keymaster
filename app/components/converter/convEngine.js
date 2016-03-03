aaa560f7d9a7ba7a90a808cabd8ac0f3ac287e09
Bert <Bert>
2016-03-03 16:12:58 -0700

    Converter is WORKING!!!!!

--- app/components/converter/convEngine.js
+++ app/components/converter/convEngine.js
@@ -2,7 +2,6 @@ app.service("ConversionEngine", function () {
     
     //This converts difficult chords (B# to C), (E# to F), (Cb to B), (Fb to E) to good stuff
     this.funkOut = function (chord) {
-        alert("funkOut inservice"); //delete this line after first run-through
         switch (chord) {
             case "B#":
                 chord = "C";
@@ -20,7 +19,6 @@ app.service("ConversionEngine", function () {
     }
 
     this.toFlat = function (chord) {
-        alert('toFlat inservice');//delete this line after first run-through
         switch (chord) {
             case "A#":
                 chord = "Bb";
@@ -41,7 +39,6 @@ app.service("ConversionEngine", function () {
     }
 
     this.toSharp = function (chord) {
-        alert('toSharp inservice');//delete this line after first run-through
         switch (chord) {
             case "Bb":
                 chord = "A#";
@@ -62,7 +59,6 @@ app.service("ConversionEngine", function () {
     }
 
     this.toGuitar = function (chord) {
-        alert('toGuitar inservice');//delete this line after first run-through
         switch (chord) {
             case "A#":
                 chord = "Bb";
@@ -81,56 +77,94 @@ app.service("ConversionEngine", function () {
         }
         return chord;
     }
-     /* pseudo-code for the "converter" function .............
-     
-     to parser --> .... done
-     tabLine == true --> .... done
-     sift "A"-"G" -->
-     ? is next char "#" or "b"
-     grab chordBlock -->
-     run funkOut --> 
-     run shifter -->
-     replace chordBlock -->
-     GOTO next chordBlock -->
-     GOTO next tabLine
-     */
+    /* pseudo-code for the "converter" function .............
+    
+    to parser --> .... done
+    tabLine == true --> .... done
+    sift "A"-"G" -->
+    ? is next char "#" or "b"
+    grab chordBlock -->
+    run funkOut --> 
+    run shifter -->
+    replace chordBlock -->
+    GOTO next chordBlock -->
+    GOTO next tabLine
+    */
+    
+    this.shifter = function(chord, slider, accidental){
+        this.spot =0
+        switch (accidental){
+            case '#':
+            this.spot = this.sharpList.indexOf(chord, 12);
+            chord = this.sharpList[this.spot + slider];
+            break;
+            case 'b':
+            this.spot = this.flatList.indexOf(chord, 12);
+            chord = this.flatList[this.spot + slider];
+            break;
+            case 'g':
+            this.spot = this.guitarList.indexOf(chord, 12);
+            chord = this.guitarList[this.spot + slider];
+            break;
+        }
+        return chord;
+    }
     //reference lists to create conversion tables from
     this.sharpList = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
     this.flatList = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab'];
     this.guitarList = ['A', 'Bb', 'B', 'C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B', 'C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B', 'C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab'];
 
-this.convert = function(tabLine, accidental){
-    
-}
-   
-    // v--- Jake's code ..................
-    this.converter = function (chord, list) {
-        for (var i = 0; i < list.length; i++) {
-            var currentChord = list[i];
-            if (chord === currentChord) {
-                //go up by 2 make sure thats okay
-                //return list[nextIndex]
+    this.convert = function (tabLine, slider, accidental) {
+        this.newTabLine = '';
+        for (var i = 0; i < tabLine.length; i++) {
+            var chord = '';
+            if (tabLine.charCodeAt(i) >= 65 && tabLine.charCodeAt(i) <= 71) {//looking for letters between A & G
+                if (tabLine.charCodeAt(i + 1) === 35 || tabLine.charCodeAt(i + 1) === 98) {//trapping the # & b characters
+                     chord = tabLine[i] + tabLine[i + 1];
+                    i++;
+                } else {
+                     chord = tabLine[i];
+                }
+                //TODO ... initiate chord-shift process here ................
+                if(chord){
+                   chord = this.funkOut(chord);
+                    switch (accidental){
+                        case '#':
+                        chord = this.toSharp(chord);
+                        break;
+                        case 'b':
+                        chord = this.toFlat(chord);
+                        break;
+                        case 'g':
+                        chord = this.toGuitar(chord);
+                        break;
+                    }
+                   chord = this.shifter(chord, slider, accidental) // TODO ... write this function ........
+                }
+                this.newTabLine += chord;
+                continue;
             }
+            this.newTabLine += tabLine[i];
         }
-    }
-
-    // v-- parsing function to distinguish chord lines from lyrics
-
+        return this.newTabLine;
+    }//end of convert function
+   
+       // v-- parsing function to distinguish chord lines from lyrics
     this.analyze = function (phrase) {
-            //trap all other capitols and return flase if found (thanks Rob)
-        for(var i=0; i < phrase.length; i++){
-        if (phrase.charCodeAt(i) < 91 && phrase.charCodeAt(i) > 71 && phrase.charCodeAt(i) != 75) {
-            return false;
-        }
+        //trap all other capitols and return flase if found (thanks Rob)
+        for (var i = 0; i < phrase.length; i++) {
+            if (phrase.charCodeAt(i) < 91 && phrase.charCodeAt(i) > 71 && phrase.charCodeAt(i) != 75) { // 75 is the letter K ... excluded for the word Key
+                return false;
+            }
         }
         // debugger;
         if (phrase.length === 1 && phrase.charCodeAt(0) != 32) {
             return true;
         }
-         if (phrase.length === 0) {
+        if (phrase.length === 0) {
             return false;
         }
-        if(phrase.search("Bridge") !=-1 || phrase.search("o") !=-1){
+        if (phrase.search("Bridge") != -1 || phrase.search("o") != -1) {
             return false;
         }
 
@@ -143,7 +177,7 @@ this.convert = function(tabLine, accidental){
         //     return false;
         // }
 
-    this.prelimParse = ['A#', 'B#', 'C#', 'D#', 'E#', 'F#', 'G#', 'Ab', 'Bb', 'Cb', 'Db', 'Eb', 'Fb', 'Gb', '2', '7', '9'];
+        this.prelimParse = ['A#', 'B#', 'C#', 'D#', 'E#', 'F#', 'G#', 'Ab', 'Bb', 'Cb', 'Db', 'Eb', 'Fb', 'Gb', '2', '7', '9'];
         var flag = 0;
         for (var i = 0; i < this.prelimParse.length; i++) {
             if (phrase.search(this.prelimParse[i]) > 0) {
@@ -153,11 +187,11 @@ this.convert = function(tabLine, accidental){
                 }
             }
         }
-            var spaceFlag = 0;
-            var sharpFlag = 0;
-            var chordFlag = 0;
-            var numFlag = 0;
-            var augDimFlag = 0;
+        var spaceFlag = 0;
+        var sharpFlag = 0;
+        var chordFlag = 0;
+        var numFlag = 0;
+        var augDimFlag = 0;
         for (var i = 0; i < phrase.length; i++) {
             switch (phrase.charCodeAt(i)) {
                 case 32:
@@ -197,10 +231,10 @@ this.convert = function(tabLine, accidental){
             return true;
         }
         // preponderance of CAPS and other spesh characters --v
-        if (((chordFlag + augDimFlag + sharpFlag + numFlag) +1) * 3 >= phrase.length) {
+        if (((chordFlag + augDimFlag + sharpFlag + numFlag) + 1) * 3 >= phrase.length) {
             return true;
         }
-    
+
         return false; //when all other tests fail
     }//end of analyze (parsing) function ---^
 
