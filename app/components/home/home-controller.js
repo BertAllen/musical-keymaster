b077eda3bdbef63380d3ab356f5e04bbc547d073
Bert Allen <mrlaughingdragon@aol.com>
2016-03-28 11:22:46 -0600

    original key variable added

--- app/components/home/home-controller.js
+++ app/components/home/home-controller.js
@@ -1,11 +1,20 @@
 app.controller('HomeController', function($rootScope, $scope, $stateParams, ConversionEngine) {
     $scope.accidental = "g";
+    $scope.goBack = 0;
     $scope.peekaboo = function() {
         if (!$scope.musicInput) {
             alert("Blank songs cannot be converted. Please input chord and lyric information before attempting to convert.");
             return;
         }
-        var originalInput = $scope.musicInput;
+        // var originalInput = $scope.musicInput;
+        $scope.goBack -= $scope.slider.value;
+        if ($scope.goBack < -11 || $scope.goBack > 11) {
+            if ($scope.goBack < -11) {
+                $scope.goBack += 12;
+            } else {
+                $scope.goBack -= 12;
+            }
+        }
         // splits original input into individual lines --v
         $scope.lineArr = $scope.musicInput.split(String.fromCharCode(10));
 
@@ -26,7 +35,7 @@ app.controller('HomeController', function($rootScope, $scope, $stateParams, Conv
         //stuff to save the song info into firebase --v
         $rootScope.member.mySongs = $rootScope.member.mySongs || {};
         $rootScope.CANSAVE = $scope.title;
-        var newSong = { "originalInput": originalInput, musicInput: $scope.musicInput, title: $scope.title };
+        var newSong = {musicInput: $scope.musicInput, title: $scope.title, netShift: $scope.goBack };
         $rootScope.member.mySongs[newSong.title] = newSong;
 
     }//end of peekaboo
@@ -45,11 +54,7 @@ app.controller('HomeController', function($rootScope, $scope, $stateParams, Conv
         return;
     }
 
-    $rootScope.revert = function() {
-        debugger;
-        $scope.musicInput = $rootScope.member.mySongs[$scope.title].originalInput;
-        return $scope.musicInput;
-}
+    
 
     $scope.downAndDirty = function() {
         $scope.newTabLine = ConversionEngine.convert($scope.tabLine, $scope.slider.value, $scope.accidental);
